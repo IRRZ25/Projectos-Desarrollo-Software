636bc04d5a918de0d4a944e4044e95a048cc0a27
IrvinRomero-dev <148299577+IRRZ25@users.noreply.github.com>
2026-03-25 13:37:38 -0500

    Create products and nike_raw tables with sample data
    Added tables for products and nike_raw, along with example data insertion and a query for high-rated products.

--- SQL/Nike_data_model.sql
+++ SQL/Nike_data_model.sql
@@ -7,3 +7,65 @@ CREATE TABLE marcas (
     id_marca INT PRIMARY KEY,
     nombre VARCHAR(100)
 );
+CREATE TABLE productos (
+    id_producto VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(150),
+    id_marca INT,
+    precio_lista DECIMAL(10,2),
+    precio_venta DECIMAL(10,2),
+    descuento DECIMAL(5,2),
+    descripcion TEXT,
+    rating DECIMAL(3,2),
+    reviews INT,
+    FOREIGN KEY (id_marca) REFERENCES marcas(id_marca)
+);
+-- Consulta: Aqui estams consultando los productos con alto rating
+SELECT 
+    p.nombre,
+    m.nombre AS marca,
+    p.precio_lista,
+    p.precio_venta,
+    p.descuento,
+    p.rating
+FROM productos p
+JOIN marcas m ON p.id_marca = m.id_marca
+WHERE p.descuento > 10
+AND p.rating >= 4;
+--Datos de el csv
+-- Tabla original basada en el CSV
+CREATE TABLE nike_raw (
+    product_name VARCHAR(150),
+    product_id VARCHAR(50),
+    listing_price DECIMAL(10,2),
+    sale_price DECIMAL(10,2),
+    discount DECIMAL(5,2),
+    brand VARCHAR(100),
+    description TEXT,
+    rating DECIMAL(3,2),
+    reviews INT,
+    images TEXT
+);
+-- Inserción de datos de ejemplo
+INSERT INTO nike_raw VALUES
+('Air Max 90', '001', 120, 100, 20, 'Nike', 'Zapato deportivo', 4.5, 150, 'img1.jpg'),
+('Air Force 1', '002', 110, 90, 18, 'Nike', 'Clásico urbano', 4.7, 200, 'img2.jpg');
+-- Insertar marcas únicas
+INSERT INTO marcas (id_marca, nombre)
+VALUES (1, 'Nike');
+
+-- Insertar productos desde tabla cruda
+INSERT INTO productos (
+    id_producto, nombre, id_marca, precio_lista, precio_venta,
+    descuento, descripcion, rating, reviews
+)
+SELECT 
+    product_id,
+    product_name,
+    1,
+    listing_price,
+    sale_price,
+    discount,
+    description,
+    rating,
+    reviews
+FROM nike_raw;
