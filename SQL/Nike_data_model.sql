59e78102ff3c21960fd4bfd7df4adaa85d2fd10d
IrvinRomero-dev <148299577+IRRZ25@users.noreply.github.com>
2026-03-25 13:29:43 -0500

    Create marcas table for Nike data model
    Create 'marcas' table for Nike data model with ID and name.

---
+++ SQL/Nike_data_model.sql
@@ -0,0 +1,9 @@
+-- Proyecto: Modelado de datos basado en dataset real (Nike)
+-- Autor: Irvin Romero
+-- Descripción:
+-- Se transforma un dataset CSV en un modelo relacional
+-- para facilitar consultas y análisis de datos.
+CREATE TABLE marcas (
+    id_marca INT PRIMARY KEY,
+    nombre VARCHAR(100)
+);
